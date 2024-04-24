57829391faf029535ade076d1221d7c77e4043fd
MelonSamuel <samuelmelon59@gmail.com>
2024-04-24 16:38:21 +0200

    ajout form registration

---
+++ SYMFONY/src/Form/RegistrationFormType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Role;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('roles', EntityType::class, [
+            'class' => Role::class,
+            'choice_label' => 'name', // Assurez-vous que l'entité Role a une propriété "name" contenant le nom du rôle
+            'multiple' => true, // Si vous permettez la sélection de plusieurs rôles
+            'expanded' => true, // Pour afficher les choix sous forme de cases à cocher
+            'label' => "Type de rôle souhaité",
+        ])
+            ->add('name')
+            ->add('firstname')
+            ->add('email')
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
