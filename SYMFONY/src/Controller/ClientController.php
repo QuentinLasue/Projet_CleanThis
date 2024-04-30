b130be6b63a298a6f37936e99d3c60b8e1d8b144
LasueQuentin <quentin.lasue.jm@gmail.com>
2024-04-30 09:30:42 +0200

    Modification structurale pour simplification,include contenue home dans base.html.twig

--- SYMFONY/src/Controller/ClientController.php
+++ SYMFONY/src/Controller/ClientController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class ClientController extends AbstractController{
     
     #[Route('/', name:'home')]
