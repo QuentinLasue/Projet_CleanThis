cea8fa514a097099b1685668cfcbc0d61e07e3c4
Shenzu0 <areis.shenzu@gmail.com>
2024-05-03 16:25:39 +0200

    Page liste presque fini

--- SYMFONY/src/DataFixtures/OperationFixtures.php
+++ SYMFONY/src/DataFixtures/OperationFixtures.php
@@ -6,41 +6,31 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Operation;
 use App\Entity\User;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class OperationFixtures extends Fixture
 {
-    private $passwordHasher;
-
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
-    {
-        $this->passwordHasher = $passwordHasher;
-    }
-
     public function load(ObjectManager $manager)
     {
         // Créer des utilisateurs avec des emails factices et des mots de passe
         for ($i = 0; $i < 3; $i++) {
             $user = new User();
             $user->setEmail('user' . $i . '@example.com'); // Remplacer par de vrais emails si nécessaire
-            // Génération d'un mot de passe sécurisé
-            $password = 'password' . $i;
-            $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
-            $user->setPassword($hashedPassword);
+            // Notez que le mot de passe n'est pas hashé ici
+            $user->setPassword('password' . $i);
             $manager->persist($user);
             $this->addReference('user_' . $i, $user);
         }
 
         // Créer des opérations associées à chaque utilisateur
         $operationsData = [
-            ['type' => 'Type 1', 'priorite' => 'Haute', 'statut' => 'En cours'],
-            ['type' => 'Type 2', 'priorite' => 'Moyenne', 'statut' => 'En cours'],
-            ['type' => 'Type 3', 'priorite' => 'Basse', 'statut' => 'En cours'],
+            ['description' => 'Description de l\'opération 1', 'statut' => 'En cours'],
+            ['description' => 'Description de l\'opération 2', 'statut' => 'En cours'],
+            ['description' => 'Description de l\'opération 3', 'statut' => 'En cours'],
         ];
 
         foreach ($operationsData as $key => $operationData) {
             $operation = new Operation();
-            $operation->setUser($this->getReference('user_' . $key));
+            $operation->setDescription($operationData['description']);
             $operation->setStatut($operationData['statut']);
             $operation->setDateForecast(new \DateTime());
             // Associer d'autres propriétés si nécessaire
