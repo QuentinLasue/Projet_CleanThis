9bb2e2b0d122f4ded2e7e2c5c654ebb7e0e7e284
Shenzu0 <areis.shenzu@gmail.com>
2024-05-14 08:21:04 +0200

    Page list terminée

--- SYMFONY/src/Controller/ListeController.php
+++ SYMFONY/src/Controller/ListeController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
-use App\Repository\OperationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\OperationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ListeController extends AbstractController
 {
-    #[Route('/liste', name: 'app_liste')]
-    public function app_operation(OperationRepository $repo): Response
+    private $entityManager;
+    private $operationRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, OperationRepository $operationRepository)
     {
-        $operationsEnCours = $repo->findBy(['statut' => 'En cours']);
-        $operationsTerminees = $repo->findBy(['statut' => 'Terminé']);
+        $this->entityManager = $entityManager;
+        $this->operationRepository = $operationRepository;
+    }
+
+    #[Route('/liste', name: 'app_liste')]
+    public function app_operation(Request $request): Response
+    {
+        // Vérifier si une opération doit être marquée comme terminée
+        if ($request->request->has('operation_id')) {
+            $entityManager = $this->entityManager;
+            // Récupérer l'ID de l'opération à terminer depuis la requête
+            $operationId = $request->request->get('operation_id');
+
+            // Récupérer l'opération en cours
+            $operation = $this->operationRepository->find($operationId);
+
+            if ($operation) {
+                // Mettre à jour le statut de l'opération en cours à "Terminé"
+                $operation->setStatut('Terminé');
+                $entityManager->flush();
+            }
+        }
+
+        $operationsEnCours = $this->operationRepository->findBy(['statut' => 'En cours']);
+        $operationsTerminees = $this->operationRepository->findBy(['statut' => 'Terminé']);
 
         return $this->render('liste.html.twig', [
             'operationsEnCours' => $operationsEnCours,
