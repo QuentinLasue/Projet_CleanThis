53458d10e868635fe1392f403ad7a6b3c439de5a
areis.letren@gmail.com <59013-47-01@exchange.ad.afpanet>
2024-05-14 13:03:54 +0200

    ee

--- SYMFONY/src/Controller/OperationController.php
+++ SYMFONY/src/Controller/OperationController.php
@@ -2,27 +2,49 @@
 
 namespace App\Controller;
 
-use App\Entity\Operation;
 use App\Repository\OperationRepository;
-use App\Repository\TypeOperationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Operation;
 
 class OperationController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/operation", name="app_operation")
      */
-    #[Route('/operation', name: "app_operation" )]
-    public function app_operation(OperationRepository $repo): Response
+    public function operation(OperationRepository $repo): Response
     {
-    
         $operations = $repo->findAll();
 
-        
         return $this->render('operation.html.twig', [
             'operations' => $operations,
         ]);
     }
+
+    /**
+     * @Route("/operation/prendre/{id}", name="app_operation_prendre")
+     */
+    public function prendreOperation(Operation $operation, Request $request): Response
+    {
+        // Traitement pour transférer l'opération à "Ma Liste"
+        // Vous pouvez mettre à jour le statut de l'opération ici
+        // et la sauvegarder dans la base de données
+
+        $operation->setStatut("En cours");
+        $this->entityManager->flush();
+
+        // Redirection vers la page "Ma Liste" après avoir pris l'opération
+        return $this->redirectToRoute('app_ma_liste');
+    }
 }
+ 
