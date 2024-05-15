f25119076980b549a8538f32f48dc7a78041e8cf
Achourimoussa <achourimoussa@gmail.com>
2024-05-15 09:45:44 +0200

    stat

--- SYMFONY/assets/app.js
+++ SYMFONY/assets/app.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-// import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-=======
+
 import './bootstrap.js';
->>>>>>> connexion
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- SYMFONY/src/Controller/StatController.php
+++ SYMFONY/src/Controller/StatController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\TypeOperation;
+use App\Repository\OperationRepository;
+use App\Repository\TypeOperationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class StatController extends AbstractController
 {
     #[Route('/stat', name: 'app_stat')]
-    public function index(): Response
+    public function index(OperationRepository $operationRepository,TypeOperationRepository $typeOperationRepository,ChartBuilderInterface $chartBuilder): Response
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $data = [];
+        $labels = [];
+
+        $TypeOperations = $typeOperationRepository ->findAll();
+        $Operations =$operationRepository ->findAll();
+
+        foreach ($TypeOperations as $TypeOperation) {
+            $data[] = $TypeOperation->getPrix();
+            
+        }
+        foreach ($Operations as $Operation) {
+            $labels[] = $Operation->getDateStart();
+            
+        }
+
+
+        $chart->setData([
+            'labels' => [],
+            'datasets' => [
+                [
+                    'label' => 'Données cleanthis',
+                    'backgroundColor' => 'rgb(200, 99, 132)',
+                    'borderColor' => 'rgb(200, 99, 132)',
+                    'data' => $data,
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'scales' => [
+                'y' => [
+                    'suggestedMin' => 0,
+                    'suggestedMax' => 100,
+                ],
+            ],
+        ]);
+
         return $this->render('stat/index.html.twig', [
-            'controller_name' => 'StatController',
+            'chart' => $chart,
         ]);
     }
+
 }
+
+
