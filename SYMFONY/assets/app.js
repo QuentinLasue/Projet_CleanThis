27e2808e20ab20e817091c48e2535f60f0db4505
Achourimoussa <achourimoussa@gmail.com>
2024-05-16 16:30:14 +0200

    modif stat

--- SYMFONY/assets/app.js
+++ SYMFONY/assets/app.js
@@ -2,6 +2,7 @@
 import './bootstrap.js';
 import './styles/app.css';
 
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
 //Connexion 
@@ -58,5 +59,4 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&scope=email%20profile&state=YOUR_STATE";
     });
 });
-
 
