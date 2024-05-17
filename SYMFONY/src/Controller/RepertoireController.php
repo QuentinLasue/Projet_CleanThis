fc5dc54376300fba075e272d80df61aa465b59d8
Nordine b <nordine20@hotmail.com>
2024-05-17 15:12:05 +0200

    operation fonctionnel sauf client

--- SYMFONY/src/Controller/RepertoireController.php
+++ SYMFONY/src/Controller/RepertoireController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\OperationRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RepertoireController extends AbstractController
 {
@@ -15,22 +15,45 @@ class RepertoireController extends AbstractController
     public function index(Request $request, OperationRepository $operationRepository, PaginatorInterface $paginator): Response
     {
         $searchField = $request->query->get('searchField');
-        $searchTerm = $request->query->get('searchTerm');
-
-        if ($searchField && $searchTerm) {
-            $operations = $operationRepository->findByFieldAndTerm($searchField, $searchTerm);
+        $searchValue = $request->query->get('searchValue');
+        
+        if ($searchField && $searchValue) {
+            switch ($searchField) {
+                case 'id':
+                    $operations = $operationRepository->findByCritereId($searchValue);
+                    break;
+                case 'statut':
+                    $operations = $operationRepository->findByCritereStatut(['statut' => $searchValue]);
+                    break;
+                case 'dateForecast':
+                    $operations = $operationRepository->findByCritereDatePrev($searchValue);
+                    break;
+                case 'client':
+                    $operations = $operationRepository->findByCritereClient($searchValue);
+                    break;
+                case 'user':
+                    $operations = $operationRepository->findByCritereUser($searchValue);
+                    break;
+                default:
+                    $operations = $operationRepository->findByFieldAndTerm($searchField, $searchValue);
+            }
         } else {
+            // Si aucun champ de recherche n'est spécifié, récupérez toutes les opérations
             $operations = $operationRepository->findAll();
         }
 
+        // Pagination des résultats
         $operations = $paginator->paginate(
             $operations,
             $request->query->getInt('page', 1),
-            5
+            4
         );
 
+        // Rendu du template avec les opérations et les valeurs de recherche
         return $this->render('repertoire/index.html.twig', [
             'operations' => $operations,
+            'searchField' => $searchField,
+            'searchValue' => $searchValue,
         ]);
     }
 }
--- SYMFONY/src/Repository/OperationRepository.php
+++ SYMFONY/src/Repository/OperationRepository.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace App\Repository;
 
@@ -13,27 +13,61 @@ class OperationRepository extends ServiceEntityRepository
         parent::__construct($registry, Operation::class);
     }
 
-    // Méthode pour rechercher par critère 1 (dans ce cas, l'ID)
-    public function findByCritere1($id)
+    public function findByCritereId($id)
     {
-        return $this->findOneBy(['id' => $id]);
+        return $this->findBy(['id' => $id]);
     }
 
-    // Méthode pour rechercher par statut
     public function findByCritereStatut($statut)
-    {
-        return $this->findBy(['statut' => $statut]);
-    }
+{
+    return $this->createQueryBuilder('operation')
+        ->andWhere('operation.statut = :statut')
+        ->setParameter('statut', $statut)
+        ->getQuery()
+        ->getResult();
+}
+public function findByCritereDatePrev($dateForecast)
+{
+    return $this->createQueryBuilder('operation')
+        ->andWhere('operation.dateForecast = :dateForecast')
+        ->setParameter('dateForecast', new \DateTime($dateForecast)) // Conversion en DateTime
+        ->getQuery()
+        ->getResult();
+}
 
-    // Méthode pour rechercher par champ spécifié et terme
-    public function findByFieldAndTerm($field, $term)
-    {
-        return $this->createQueryBuilder('operation')
-            ->leftJoin('operation.user', 'user') // Assurez-vous que la relation avec l'utilisateur est correctement définie
-            ->andWhere("operation.$field = :term") // Utilisez le bon champ pour la condition
-            ->orWhere("user.name = :term") // Ou utilisez le nom de l'utilisateur comme critère de recherche
-            ->setParameter('term', $term)
-            ->getQuery()
-            ->getResult();
-    }
+public function findByCritereClient($client)
+{
+    dump($client);
+    return $this->createQueryBuilder('operation')
+        ->join('operation.client', 'client')
+        ->andWhere('client.name LIKE :clientName OR client.firstname LIKE :clientName')
+        ->setParameter('clientName', '%' . $client . '%')
+        ->getQuery()
+        ->getResult();
+}
+
+public function findByCritereUser($user)
+{
+    return $this->createQueryBuilder('operation')
+        ->join('operation.user', 'user')
+        ->andWhere('user.name LIKE :userName OR user.firstname LIKE :userName')
+        ->setParameter('userName', '%' . $user . '%')
+        ->getQuery()
+        ->getResult();
+}
+
+public function findByFieldAndTerm($field, $term)
+{
+    return $this->createQueryBuilder('operation')
+        ->leftJoin('operation.user', 'user')
+        ->leftJoin('operation.client', 'client')
+        ->andWhere("operation.$field LIKE :term")
+        ->orWhere("user.name LIKE :term")
+        ->orWhere("user.firstname LIKE :term")
+        ->orWhere("client.name LIKE :term")
+        ->orWhere("client.firstname LIKE :term")
+        ->setParameter('term', '%' . $term . '%')
+        ->getQuery()
+        ->getResult();
+}
 }
