f9944015df23fbcb300cf9d1268dd48700afa84e
LasueQuentin <quentin.lasue.jm@gmail.com>
2024-05-21 14:57:20 +0200

    commentaire import bootstrap

--- SYMFONY/assets/app.js
+++ SYMFONY/assets/app.js
@@ -1,5 +1,5 @@
 
-import './bootstrap.js';
+// import './bootstrap.js';
 import './styles/app.css';
 
 // console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
@@ -65,23 +65,23 @@ import './styles/app.css';
     //     window.location.href = "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&scope=email%20profile&state=YOUR_STATE";
     // });
 
-    //Pour la modal tu footer : 
-    // on cible la modal et son icon de fermeture et le contenue de la modal
-    var modal = document.getElementById("openModal");
-    var close = document.getElementById("closeModal");
-    var modalContent = document.getElementById("myModal");
+//Pour la modal tu footer : 
+// on cible la modal et son icon de fermeture et le contenue de la modal
+var modal = document.getElementById("openModal");
+var close = document.getElementById("closeModal");
+var modalContent = document.getElementById("myModal");
 
-    //on ajoute les événement d'apparition et disparition
-    modal.onclick = function openModal() {
-        modalContent.style.display = "block";
+//on ajoute les événement d'apparition et disparition
+modal.onclick = function openModal() {
+    modalContent.style.display = "block";
+}
+
+close.onclick =function closeModal() {
+    modalContent.style.display = "none";
+}
+// ferme la modal quand l'utilisateur clique en dehors de la modal
+window.onclick = function(event) {
+    if (event.target == modalContent) {
+        modalContent.style.display = 'none';
     }
-    
-    close.onclick =function closeModal() {
-        modalContent.style.display = "none";
-    }
-    // ferme la modal quand l'utilisateur clique en dehors de la modal
-    window.onclick = function(event) {
-        if (event.target == modalContent) {
-            modalContent.style.display = 'none';
-        }
-    }
+}
