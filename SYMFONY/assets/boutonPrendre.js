83b547eff08bda97dcfb09ab2a7a1875f0b333d4
Shenzu0 <areis.shenzu@gmail.com>
2024-05-27 11:10:28 +0200

    boutonPrendre

--- SYMFONY/assets/boutonPrendre.js
+++ SYMFONY/assets/boutonPrendre.js
@@ -1,34 +1,34 @@
-
-const prendreButtons = document.querySelectorAll('.prendre-button');
-
-
-prendreButtons.forEach(button => {
-    button.addEventListener('click', function(event) {
-     
-        event.preventDefault();
-        
-       
-        const formUrl = this.closest('.operation-form').getAttribute('action');
-        
-       
-        fetch(formUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => {
-           
-            if (!response.ok) {
-                throw new Error('Erreur lors de la prise de l\'opération.');
-            }
-            
-           
-            this.closest('.custom-block').remove();
-        })
-        .catch(error => {
-            console.error('Erreur:', error);
+document.addEventListener("DOMContentLoaded", () => {
+    const prendreButtons = document.querySelectorAll('.prendre-button');
+    
+    
+    prendreButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+    
+            event.preventDefault();
+    
+    
+            const formUrl = this.closest('.operation-form').getAttribute('action');
+    
+    
+            fetch(formUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(response => {
+    
+                if (!response.ok) {
+                    throw new Error('Erreur lors de la prise de l\'opération.')
+                }
+    
+    
+                this.closest('.custom-block').remove();
+            })
+            .catch(error => {
+                console.error('Erreur:', error);
+            });
         });
-
     });
-});
+    })
