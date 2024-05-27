45321eb9f156f32e5d41da2994b4951d0c199145
Shenzu0 <areis.shenzu@gmail.com>
2024-05-27 11:02:06 +0200

    js boutprendre

--- SYMFONY/assets/app.js
+++ SYMFONY/assets/app.js
@@ -61,39 +61,6 @@ import './styles/app.css';
 // });
     
 
-const prendreButtons = document.querySelectorAll('.prendre-button');
-
-
-prendreButtons.forEach(button => {
-    button.addEventListener('click', function(event) {
-     
-        event.preventDefault();
-        
-       
-        const formUrl = this.closest('.operation-form').getAttribute('action');
-        
-       
-        fetch(formUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => {
-           
-            if (!response.ok) {
-                throw new Error('Erreur lors de la prise de l\'opération.');
-            }
-            
-           
-            this.closest('.custom-block').remove();
-        })
-        .catch(error => {
-            console.error('Erreur:', error);
-        });
-
-    });
-});
 
 // //menu burger
 
